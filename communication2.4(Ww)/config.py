c859ecb5750811a4d23e1a719193570c6bb39f4e
SeenMercury <finalsavef42024@163.com>
2025-10-12 20:25:53 +0800

    测试1

---
+++ communication2.4(Ww)/config.py
@@ -0,0 +1,11 @@
+apps = [
+    'D:/Communication/DingDing/DingtalkLauncher.exe',
+    'D:/Communication/QQ/QQ.exe',
+    'D:/Communication/.com/MailMaster/Application/mailmaster.exe'
+]
+
+programs0 = ["DingTalk.exe", "QQ.exe", "mailmaster.exe"]
+
+programs1 = ["DingTalk.exe", "QQ.exe"]
+
+time0 = 17
