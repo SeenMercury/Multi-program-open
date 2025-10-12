5cc4df0c40054dc02624ca414669bd4f03c61017
SeenMercury <finalsavef42024@163.com>
2025-10-12 14:52:26 +0800

    老东西

---
+++ 历史文件/communication0.0.bat
@@ -0,0 +1,10 @@
+@echo off
+echo "Earth online S3 Ongoing"
+rem
+@echo off
+echo Bother........ Don't want to go to work
+rem
+start "" "D:\Communication\WeChat\WeChat.exe
+choice /t 3 /d y /n >null
+start "" "D:\Communication\DingDing\DingtalkLauncher.exe
+start "" "D:\Communication\QQ\QQ.exe
---
+++ 历史文件/番外steam启动-0.1.bat
@@ -0,0 +1,3 @@
+start "" "E:\steam\Steam.exe
+start "" "C:\Users\l2978\Desktop\AccUpdater
+start "" "E:\UU\Netease\UU\uu.exe
